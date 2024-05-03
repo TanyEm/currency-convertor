25125d5b7298e771a26312b7687a91ba1293465c
Tatiana Podlesnykh <tanyem@protonmail.com>
2024-05-03 17:37:14 +0300

    Read query parameters from fields, support error presentation

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,38 +7,58 @@ const currencyConvertorAPI = {
         url.searchParams.append('target_currency', targetCurrency);
         url.searchParams.append('monetary_value', monetaryValue);
 
-        return fetch(url).then(response => response.json());
+        return fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(error => {
+                        throw error;
+                    });
+                }
+                return response.json();
+            });
     }
 }
 
-const RateResult = {
-    data() {
-        return {
-            rate: null
-        }
-    },
-    template: '<div>{{rate}}</div>',
-    created() {
-        currencyConvertorAPI.fetch('EUR', 'USD', 100)
-            .then(data => {
-                this.rate = data.result;
-            })
-            .catch(error => console.error('Error:', error));
-    }
+const ConversionResult = {
+    props: ['rate'],
+    template: '<div>{{rate}}</div>'
 }
 
 const app = createApp({
     data() {
         return {
-            rate: {}
+            rate: '',
+            sourceCurrency: '',
+            targetCurrency: '',
+            monetaryValue: '',
+            errorMessage: ''
         }
     },
     components: {
-        RateResult
+        ConversionResult
+    },
+    methods: {
+        fetchRate() {
+            currencyConvertorAPI.fetch(this.sourceCurrency, this.targetCurrency, this.monetaryValue)
+                .then(data => {
+                    this.rate = data.result;
+                    this.errorMessage = '';
+                })
+                .catch(error => {
+                    this.rate = '';
+                    this.errorMessage = Object.values(error.errors).flat().join('. ');
+                    console.error('Error:', error);
+                });
+        }
     },
     template: `
         <div>
-            <RateResult :rate="rate"/>
+            <input v-model="sourceCurrency" placeholder="Source Currency">
+            <input v-model="targetCurrency" placeholder="Target Currency">
+            <input v-model="monetaryValue" placeholder="Monetary Value">
+            <button @click="fetchRate">Fetch Rate</button>
+            <ConversionResult :rate="rate"/>
+            <div v-if="errorMessage">{{errorMessage}}</div>
         </div>
     `
 })
